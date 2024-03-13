37fe7b49977a91b29fd14dc8293d7594216fe912
StephenW9527 <log9527@gmail.com>
2024-03-13 21:16:25 +0800

    some basic codes written in java  practices first commit.

---
+++ src/main/java/tw/com/hkt/Main.java
@@ -0,0 +1,31 @@
+package tw.com.hkt;
+
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+//    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public static void main(String[] args) {
+
+//            Scanner sc = new Scanner(System.in);
+//
+//            System.out.print("請輸入體重");
+//            double weight = sc.nextDouble();
+//
+//            System.out.print("請輸入身高");
+//            double height = sc.nextDouble();
+
+//        String dinner = JOptionPane.showInputDialog("晚餐想吃甚麼??");  產生一個輸入框
+//        JOptionPane.showMessageDialog(null,dinner);                   將輸入的文字顯示出來
+
+        String[] Countries = {"Japan", "America", "China", "Korea", "Taiwan", "UK", "France" };
+
+
+
+
+    }
+}
