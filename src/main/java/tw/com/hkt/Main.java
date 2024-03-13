3c870a71da61d1067c1917f1934ade1a50e6266a
StephenW9527 <log9527@gmail.com>
2024-03-13 21:46:39 +0800

    3rd commit

--- src/main/java/tw/com/hkt/Main.java
+++ src/main/java/tw/com/hkt/Main.java
@@ -4,6 +4,8 @@ package tw.com.hkt;
 import javax.swing.*;
 import java.lang.reflect.Array;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -22,8 +24,27 @@ public class Main {
 //        String dinner = JOptionPane.showInputDialog("晚餐想吃甚麼??");  產生一個輸入框
 //        JOptionPane.showMessageDialog(null,dinner);                   將輸入的文字顯示出來
 
-        String[] Countries = {"Japan", "America", "China", "Korea", "Taiwan", "UK", "France" };
 
+        //方式1
+        String[] countries = {"Japan", "America", "China", "Korea", "Taiwan", "UK", "France" };
+        System.out.println(Arrays.toString(countries));  //印出整個Array
+        System.out.println(countries);  //印出Array在記憶體的位址
+
+
+        //方式2
+        String[] countries2 = new String[7];
+        countries2[0] = "Japan";
+        countries2[1] = "America";
+        countries2[2] = "China";
+        countries2[3] = "Korea";
+        countries2[4] = "Taiwan";
+        countries2[5] = "UK";
+        countries2[6] = "France";
+        System.out.println(Arrays.toString(countries2));
+
+
+        //動態的陣列
+        ArrayList<String> countries3 = new ArrayList<>();
 
 
 
